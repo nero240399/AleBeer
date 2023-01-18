4ec0766943842ab4189835f18a4079aab450490b
Nero <hungnd.vnse@gmail.com>
2023-01-18 22:25:35 +0700

    fix : call api to get images "again" if some were error -> "only once"

--- app/src/main/java/com/example/alebeer/AleBeerApplication.kt
+++ app/src/main/java/com/example/alebeer/AleBeerApplication.kt
@@ -1,7 +1,21 @@
 package com.example.alebeer
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AleBeerApplication : Application()
+class AleBeerApplication : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .networkCachePolicy(CachePolicy.WRITE_ONLY)
+            .respectCacheHeaders(false)
+            .build()
+    }
+}
